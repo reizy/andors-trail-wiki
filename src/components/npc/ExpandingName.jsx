d1db48f0dd42053e313c1da3896cd10dd75012ed
teacher144123 <teacher144123@gmail.com>
2022-01-01 21:37:15 +0800

    auto expaneded when anchor is triggered

--- src/components/npc/ExpandingName.jsx
+++ src/components/npc/ExpandingName.jsx
@@ -23,7 +23,7 @@ export default class ExpandingName extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            expanded: false,
+            expanded: window.location.href.endsWith(this.props.data.id.toLowerCase()),
         }
         this.toggleExpand = this.toggleExpand.bind(this);
     }
